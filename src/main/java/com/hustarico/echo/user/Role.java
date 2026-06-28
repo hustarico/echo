d25103853aa09f5d05a1bd02297580329ccbc22d
hustarico <hussamtarteer74@gmail.com>
2026-06-28 14:37:35 +0300

    user class :done?

---
+++ src/main/java/com/hustarico/echo/user/Role.java
@@ -0,0 +1,7 @@
+package com.hustarico.echo.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
--- src/main/java/com/hustarico/echo/user/User.java
+++ src/main/java/com/hustarico/echo/user/User.java
@@ -4,6 +4,7 @@ package com.hustarico.echo.user;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -23,11 +24,23 @@ public class User implements UserDetails {
     private int id;
     private String username;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
 
     @Override
+    public String getUsername(){
+        return username;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
