292c55feffe9c829d6e37e01945bf46205d9a95b
hustarico <hussamtarteer74@gmail.com>
2026-06-30 14:49:50 +0300

    jwtService: added methods for generating jwt w/extraClaims or not

--- src/main/java/com/hustarico/echo/config/JwtService.java
+++ src/main/java/com/hustarico/echo/config/JwtService.java
@@ -2,12 +2,17 @@ package com.hustarico.echo.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -15,18 +20,39 @@ public class JwtService {
 
     private static final String secretKey = "452af32e25a174dcfcbcdf760a52a2645635e2bd0a3964f2b844b1ddd17a65d9";
 
+
+    public String generateToken(
+            UserDetails userDetails
+    ){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    public String generateToken(
+            Map<String,Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + (1000*60*60*24) ))
+                .signWith(getSigningKey(),Jwts.SIG.HS256)
+                .compact();
+    }
+
+
+
+
     public String extractUsername(String jwt){
 
         return extractClaim(jwt,Claims::getSubject); 
     }
 
-
     public <T> T extractClaim(String jwt, Function<Claims,T> claimsResolver){
         final Claims claims = extractAllClaims(jwt);
         return claimsResolver.apply(claims);
     }
-
-
 
     public Claims extractAllClaims(String jwt){
 
