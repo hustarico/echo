678412622bdaf39a9301db33a73c5b9737c4f693
hustarico <hussamtarteer74@gmail.com>
2026-07-01 16:27:16 +0300

    filterchain: add filterchain with configs to apply jwt filter first, applicationConfig:add authProvider bean

--- src/main/java/com/hustarico/echo/config/ApplicationConfig.java
+++ src/main/java/com/hustarico/echo/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import com.hustarico.echo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,6 +20,13 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService(){
         return username -> userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
 }
