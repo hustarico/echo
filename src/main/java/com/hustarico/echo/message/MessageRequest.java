2e0b4d5229a47540a1d3b039666aaf1f9390a462
hustarico <hussamtarteer74@gmail.com>
2026-07-19 15:26:22 +0300

    adding some more validation

--- src/main/java/com/hustarico/echo/message/MessageRequest.java
+++ src/main/java/com/hustarico/echo/message/MessageRequest.java
@@ -4,7 +4,7 @@ package com.hustarico.echo.message;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record MessageRequest(@NotBlank @Size(min = 1, max= 10) String text, String sentTo) {
+public record MessageRequest(@NotBlank @Size(min = 1, max= 50) String text, @NotBlank String sentTo) {
 
 
 }
